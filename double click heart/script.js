e7aa0eb646d78444e3f5dd4b804f069df9e7fb71
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-05-23 08:23:45 +0530

    double click to like and heart appearing effect

---
+++ double click heart/script.js
@@ -0,0 +1,42 @@
+const loveme = document.querySelector(".loveme")
+const times = document.querySelector("#times")
+
+let clickTimes = 0
+let timesClicked = 0
+
+loveme.addEventListener("click",(e)=>{
+    if(clickTimes === 0){
+        clickTimes = new Date().getTime()
+    }else{
+        if((new Date().getTime() - clickTimes)<800){
+            createHeart(e)
+            clickTimes = 0
+        }else{
+            clickTimes = new Date().getTime()
+        }
+    }
+})
+
+const createHeart = (e) =>{
+    const heart = document.createElement('i')
+    heart.classList.add("fas")
+    heart.classList.add("fa-heart")
+    const x = e.clientX
+    const y = e.clientY
+
+    const leftOffset = e.target.offsetLeft
+    const topOffset = e.target.offsetTop
+
+    const xInside = x - leftOffset
+    const yInside = y - topOffset
+
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+    loveme.appendChild(heart)
+
+    times.innerHTML = ++timesClicked
+
+    setTimeout(()=> heart.remove(),1000)
+
+}
